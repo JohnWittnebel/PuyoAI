1e0a47433f1e58d17be0a89dd74086862ceeceaa
jwittneb <wittnebeljohn@gmail.com>
2023-11-11 00:20:01 -0700

    implemented scoring, fixed dropping bug

--- game.py
+++ game.py
@@ -8,10 +8,10 @@ class Game:
         self.points = 0
         self.grid = np.zeros((self.maxY,self.maxX))
 
-
     def printBoard(self):
         for row in self.grid:
             print(row)
+        print(self.points)
 
     def dropSoloPuyo(self, x, color):
         yIndex = 0
@@ -26,10 +26,49 @@ class Game:
         self.dropSoloPuyo(x1, color1)
         self.dropSoloPuyo(x1+1, color2)
 
+        pointMultiplier = 1
+        numPopped = self.popLargePuyoGroups()
+        while (numPopped > 0):
+            self.points += (numPopped * pointMultiplier)
+            pointMultiplier *= 3
+            self.poppingAftermath()
+            numPopped = self.popLargePuyoGroups()
+
     # for dropping a puyo in a vertical formation, color1 above color2
     def dropVerticalPuyo(self, x, color1, color2):
         self.dropSoloPuyo(x, color2)
         self.dropSoloPuyo(x, color1)
+        
+        pointMultiplier = 1
+        numPopped = self.popLargePuyoGroups()
+        while (numPopped > 0):
+            self.points += (numPopped * pointMultiplier)
+            pointMultiplier *= 3
+            self.poppingAftermath()
+            numPopped = self.popLargePuyoGroups()
+
+
+    # function that adjusts the grid after a group of puyos has been popped. In particular, drops the puyos that are 
+    # now hanging in the air
+    def poppingAftermath(self):
+        for xIndex in range(len(self.grid[0])):
+            yIndex = 0
+            numZeroes = 0
+            temp = []
+            while yIndex < self.maxY:
+                if self.grid[yIndex][xIndex] == 0:
+                    numZeroes += 1
+                else:
+                    temp.append(self.grid[yIndex][xIndex])
+                yIndex += 1
+            yIndex = 0
+            while yIndex < self.maxY:
+                if numZeroes > 0:
+                    self.grid[yIndex][xIndex] = 0
+                else:
+                    self.grid[yIndex][xIndex] = temp[numZeroes * -1]
+                numZeroes -= 1
+                yIndex += 1
 
     # Goes through every index running BFS to find the group size, pops if above the threshold.
     # Appears like it could take O(n^2m^2) time but if we amortize it only takes O(nm) time
@@ -38,11 +77,13 @@ class Game:
         xIndex = 0
         currGroup = []
         currGroupColor = 0
+        totalPopped = 0
         while xIndex < self.maxX and yIndex < self.maxY:
             if self.grid[yIndex][xIndex] != 0:
                 currGroupColor = self.grid[yIndex][xIndex]
                 currGroup = self.findAdjacentGroup(currGroupColor, yIndex, xIndex)
                 if len(currGroup) >= self.MAX_GROUP_SIZE:
+                    totalPopped += len(currGroup)
                     for ele in currGroup:
                         self.grid[ele[0]][ele[1]] = 0
             if xIndex + 1 == self.maxX:
@@ -50,6 +91,7 @@ class Game:
                 yIndex += 1
             else:
                 xIndex += 1
+        return totalPopped
   
     # The actual BFS that is called as a helper to popLargePuyoGroups
     def findAdjacentGroup(self, currGroupColor, yIndex, xIndex):
@@ -80,9 +122,12 @@ class Game:
 
 
 X = Game()
-X.dropHorizontalPuyo(2,3,4)
-X.dropHorizontalPuyo(1,1,1)
-X.dropHorizontalPuyo(1,1,1)
-X.popLargePuyoGroups()
-#X.findAdjacentGroup(1,11,1)
+X.dropHorizontalPuyo(2,1,1)
+X.dropHorizontalPuyo(2,1,2)
+X.dropHorizontalPuyo(2,2,1)
+X.dropHorizontalPuyo(4,2,2)
+X.dropHorizontalPuyo(4,3,3)
+X.dropHorizontalPuyo(4,2,3)
+X.dropVerticalPuyo(5,2,3)
+
 X.printBoard()
